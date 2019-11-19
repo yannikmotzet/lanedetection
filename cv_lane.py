3b1aa65b801bf06e5632625979a72a3402fd0424
ymotzet <yannik.motzet@outlook.com>
2019-11-19 21:38:14 +0100

    implemented perspective transformation

--- cv_lane.py
+++ cv_lane.py
@@ -1,32 +1,57 @@
 # date: 13.11.2019
-# username: mageejo
 # name: Yannik Motzet
 # description: a basic program to illustrate lane contours
 
-#sample image from wikipedia.org: https://en.wikipedia.org/wiki/Types_of_road#/media/File:Road_in_Norway.jpg
+import cv2
+import numpy as np
 
+# video source
+cap = cv2.VideoCapture('lane_detection.mp4')
 
-import cv2 as cv
+picHeight = 960
+picWidth = 1280
 
-#read image from source
-raw = cv.imread("Road_in_Norway.jpg")
+while(cap.isOpened()):
+    # capture frame-by-frame
+    _, frame = cap.read()
 
-#convert raw picture to greyscale
-grey = cv.cvtColor(raw, cv.COLOR_BGR2GRAY)
+    # illustrate vertices for perspective transformation
+    # point top left
+    cv2.circle(frame, (20, 300), 5, (255, 0, 0), -1)
+    # point top right
+    cv2.circle(frame, (picWidth-20, 300), 5, (0, 255, 0), -1)
+    # point bottom left
+    cv2.circle(frame, (0, picHeight), 5, (255, 0, 0), -1)
+    # point bottom right
+    cv2.circle(frame, (picWidth, picHeight), 5, (0, 0, 255), -1)
 
-#convert greyscale img to binary img
-_, binary = cv.threshold(grey, 150, 255, cv.THRESH_BINARY)
+    # perspective transformation
+    pts1 = np.float32([[0, 300], [picWidth, 300], [
+                      0, picHeight], [picWidth, picHeight]])
+    pts2 = np.float32([[0, 0], [645, 0], [290, 480], [645-290, 480]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
-#detect contours in binary img
-contours, _ = cv.findContours(binary, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    result = cv2.warpPerspective(frame, matrix, (645, 480))
+    contour = result.copy()
 
-#approximate coutours with function and draw contours to raw pimg
-for cnt in contours:
-    approx = cv.approxPolyDP(cnt, 0.01*cv.arcLength(cnt, True), True)
-    cv.drawContours(raw, [approx], 0, (0), 5)
+    # contour detection
+    grey = cv2.cvtColor(contour, cv2.COLOR_RGB2GRAY)
+    _, threshold = cv2.threshold(grey, 200, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(
+        threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # draw contours
+    for cnt in contours:
+        cv2.drawContours(contour, [cnt], 0, (0, 0, 255), 3)
 
-#show images in windows
-cv.imshow("Raw", raw)
-cv.imshow("Binary", binary)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    # output windows
+    cv2.imshow('input', frame)
+    cv2.imshow('binary', threshold)
+    cv2.imshow('perspective transformation', result)
+    cv2.imshow('contours', contour)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+# cv2.waitKey(0)
+cv2.destroyAllWindows()
